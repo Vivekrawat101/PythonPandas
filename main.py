6aafcd8aa788e170b515a0b363905f5fe3cbee17
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2022-03-04 12:38:59 +0530

    removed the commented code

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ my_data = [10, 20, 30]
 arr = np.array(my_data)
 d = {'a': 10, 'b': 20, 'c': 30}
 
-#pd.Series(data=my_data, index=labels)
-#print(pd.Series(arr, labels))
-#print(pd.Series(d))
+pd.Series(data=my_data, index=labels)
+print(pd.Series(arr, labels))
+print(pd.Series(d))
 #adding two Series
-# a = pd.Series(['India', 'USA', 'Russia', 'Canada'], ['a', 'b', 'c', 'd'])
-# b = pd.Series(['India', 'USA', 'Pakistan', 'Ukraine'], ['a', 'b', 'c', 'd'])
-# print(a+b)
-# f = pd.Series([1, 2, 3, 4], ['India', 'USA', 'Russia', 'Canada'])
-# g = pd.Series([1, 2, 3, 4], ['India', 'USA', 'Pakistan', 'Ukraine'])
-# print(f+g)
+a = pd.Series(['India', 'USA', 'Russia', 'Canada'], ['a', 'b', 'c', 'd'])
+b = pd.Series(['India', 'USA', 'Pakistan', 'Ukraine'], ['a', 'b', 'c', 'd'])
+print(a+b)
+f = pd.Series([1, 2, 3, 4], ['India', 'USA', 'Russia', 'Canada'])
+g = pd.Series([1, 2, 3, 4], ['India', 'USA', 'Pakistan', 'Ukraine'])
+print(f+g)
 
 #DataFrames----------
 
