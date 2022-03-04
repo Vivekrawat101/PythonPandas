9c63b16ca2ca2946c47f782e422f412af23c5a06
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2022-03-04 12:34:22 +0530

    Pandas Series

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+# Series-------------
+labels = ['a', 'b', 'c']
+my_data = [10, 20, 30]
+arr = np.array(my_data)
+d = {'a': 10, 'b': 20, 'c': 30}
+
+#pd.Series(data=my_data, index=labels)
+#print(pd.Series(arr, labels))
+#print(pd.Series(d))
+#adding two Series
+# a = pd.Series(['India', 'USA', 'Russia', 'Canada'], ['a', 'b', 'c', 'd'])
+# b = pd.Series(['India', 'USA', 'Pakistan', 'Ukraine'], ['a', 'b', 'c', 'd'])
+# print(a+b)
+# f = pd.Series([1, 2, 3, 4], ['India', 'USA', 'Russia', 'Canada'])
+# g = pd.Series([1, 2, 3, 4], ['India', 'USA', 'Pakistan', 'Ukraine'])
+# print(f+g)
+
+#DataFrames----------
+
+
